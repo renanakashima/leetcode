b0cfced93faaaece313026bf189f45bf9fe279c1
Rena Nakashima <88516543+renanakashima@users.noreply.github.com>
2024-10-19 11:01:42 +0200

    Time: 21 ms (91.75%) | Memory: 11.6 MB (75.68%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -6,29 +6,10 @@ class Solution(object):
         """
         dictionary = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         num = 0
-        i = len(s) - 1
-        while(i >= 0):
-            num += dictionary[s[i]]
-            if(s[i] == "V" or s[i] == "X") and i > 0:
-                if(s[i-1] == "I"):
-                    num -= 1
-                    i -= 2
-                else:
-                    i -= 1
-            elif(s[i] == "L" or s[i] == "C") and i > 0:
-                if(s[i-1] == "X"):
-                    num -= 10
-                    i -= 2
-                else:
-                    i -= 1
-            elif(s[i] == "D" or s[i] == "M") and i > 0:
-                if(s[i-1] == "C"):
-                    num -= 100
-                    i -= 2
-                else:
-                    i -= 1
-            elif(s[i] == "I") and i > 0:
-                i -= 1
+        for i in range(len(s)):
+            if(i + 1 < len(s)) and (dictionary[s[i]] < dictionary[s[i+1]]):
+                num -= dictionary[s[i]]
+
             else:
-                break
+                num += dictionary[s[i]]
         return num
