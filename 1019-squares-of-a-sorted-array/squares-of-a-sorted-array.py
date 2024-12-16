af35476d29fda13a3c90ad55f3a6c5787d458dc5
Rena Nakashima <88516543+renanakashima@users.noreply.github.com>
2024-12-16 12:46:53 +0100

    Time: 14 ms (42.15%) | Memory: 14.4 MB (5.78%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         sq = []
         for i in range(0,len(nums)):
-            sq.append(nums[i])
+            sq.append(0)
             sq[i] = nums[i] ** 2
         
         sq.sort()
